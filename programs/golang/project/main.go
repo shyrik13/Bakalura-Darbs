29b3df8de4e912286b895691df6fddf5a256b220
shyrik13 <shyrik13@inbox.lv>
2020-04-04 20:16:47 +0300

    golang multiple objects

--- programs/golang/project/main.go
+++ programs/golang/project/main.go
@@ -13,6 +13,7 @@ import (
 	_ "image/png"
 	"log"
 	"math"
+	"math/rand"
 	"runtime"
 )
 
@@ -60,12 +61,18 @@ func main() {
 	}
 
 	program, err := common.NewProgram(common.FileInString("vertex_shader.txt", 0), common.FileInString("fragment_shader.txt", 0))
-	myObject := object.New(vertices, textures, normals, textureDiff, textureNorm, program)
+	myObject := object.New(vertices, textures, normals)
+	myObject.X = -15.0
+	myObject.Y = 0.0
+	myObject.Z = -15.0
 
-	projection := mgl32.Perspective(mgl32.DegToRad(45.0), 4/3, 0.1, 100.0)
+	var objects []*object.Object
+	objects = append(objects, myObject)
+
+	projection := mgl32.Perspective(mgl32.DegToRad(90.0), 4/3, 0.1, 100.0)
 	projectionID := gl.GetUniformLocation(program, gl.Str("perspective\x00"))
 
-	view := mgl32.LookAtV(mgl32.Vec3{4, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
+	view := mgl32.LookAtV(mgl32.Vec3{1, 0, 1}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
 	viewID := gl.GetUniformLocation(program, gl.Str("view\x00"))
 
 	modelID := gl.GetUniformLocation(program, gl.Str("model\x00"))
@@ -77,37 +84,94 @@ func main() {
 	textureNormID := gl.GetUniformLocation(program, gl.Str("normal_tex\x00"))
 
 	// VERTEX DATA CONFIGURE
-	var vao uint32
-	gl.GenVertexArrays(1, &vao)
-	gl.BindVertexArray(vao)
 
-	var vboV uint32
-	gl.GenBuffers(1, &vboV)
+	myObject.InitGlObject(myObject)
 
-	var vboT uint32
-	gl.GenBuffers(1, &vboT)
-
-	var vboN uint32
-	gl.GenBuffers(1, &vboN)
-
-	defer gl.DeleteTextures(1, &myObject.TextureDiff)
-	defer gl.DeleteTextures(1, &myObject.TextureNorm)
-	defer gl.DeleteBuffers(1, &vboV)
-	defer gl.DeleteBuffers(1, &vboT)
-	defer gl.DeleteBuffers(1, &vboN)
-	defer gl.DeleteBuffers(1, &vao)
-	defer gl.DeleteProgram(myObject.Program)
+	defer gl.DeleteTextures(1, &textureDiff)
+	defer gl.DeleteTextures(1, &textureNorm)
+	defer gl.DeleteProgram(program)
 
 	var t = 0.0
 	var c = math.Cos(t)
 	var s = math.Sin(t)
 
+	var nbFrames = 0
+	var lastTime = glfw.GetTime()
+
+	var max = float32(15.0)
+	var min = float32(-15.0)
+
+	var max0 = float32(0.0)
+	var min0 = float32(-30.0)
+
 	setupScene()
 	for !window.ShouldClose() {
 
+		var currentTime = glfw.GetTime()
+		nbFrames++
+		if currentTime-lastTime >= 1.0 {
+			fmt.Printf("%f ms/frame \n", 1000.0/(float32(nbFrames)))
+			nbFrames = 0
+			lastTime += 1.0
+
+			obj := object.New(vertices, textures, normals)
+			obj.X = min0 + rand.Float32()*(max0-min0)
+			obj.Y = min + rand.Float32()*(max-min)
+			obj.Z = -25.0
+
+			objects = append(objects, obj)
+			obj.InitGlObject(obj)
+
+		}
+
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 		gl.ClearColor(0.0, 0.0, 0.4, 0.0)
-		gl.UseProgram(myObject.Program)
+
+		for _, obj := range objects {
+
+			gl.UseProgram(program)
+			gl.BindVertexArray(obj.VertexArrayID)
+
+			obj.InitGlObjectModel(obj, c, s)
+
+			gl.UniformMatrix4fv(projectionID, 1, false, &projection[0])
+			gl.UniformMatrix4fv(viewID, 1, false, &view[0])
+			gl.UniformMatrix4fv(modelID, 1, false, &obj.Model[0])
+			gl.Uniform3fv(uLightID, 1, &uLight[0])
+
+			gl.ActiveTexture(gl.TEXTURE0)
+			gl.BindTexture(gl.TEXTURE_2D, textureDiff)
+			gl.Uniform1i(textureDiffID, 0)
+
+			gl.ActiveTexture(gl.TEXTURE1)
+			gl.BindTexture(gl.TEXTURE_2D, textureNorm)
+			gl.Uniform1i(textureNormID, 1)
+
+			vertAttrib := uint32(gl.GetAttribLocation(program, gl.Str("position\x00")))
+			gl.BindBuffer(gl.ARRAY_BUFFER, obj.Vertexbuffer)
+			gl.EnableVertexAttribArray(vertAttrib)
+			gl.VertexAttribPointer(vertAttrib, 3, gl.FLOAT, false, 0, gl.PtrOffset(0))
+
+			txtAttrib := uint32(gl.GetAttribLocation(program, gl.Str("tex_coords\x00")))
+			gl.BindBuffer(gl.ARRAY_BUFFER, obj.Uvbuffer)
+			gl.EnableVertexAttribArray(txtAttrib)
+			gl.VertexAttribPointer(txtAttrib, 2, gl.FLOAT, false, 0, gl.PtrOffset(0))
+
+			normAttrib := uint32(gl.GetAttribLocation(program, gl.Str("normal\x00")))
+			gl.BindBuffer(gl.ARRAY_BUFFER, obj.Unbuffer)
+			gl.EnableVertexAttribArray(normAttrib)
+			gl.VertexAttribPointer(normAttrib, 3, gl.FLOAT, false, 0, gl.PtrOffset(0))
+
+			gl.DrawArrays(gl.TRIANGLES, 0, int32(len(obj.VertexArray)))
+
+			gl.DisableVertexAttribArray(vertAttrib)
+			gl.DisableVertexAttribArray(txtAttrib)
+			gl.DisableVertexAttribArray(normAttrib)
+
+		}
+
+		window.SwapBuffers()
+		glfw.PollEvents()
 
 		t += 0.001
 		if t > 360 {
@@ -116,53 +180,15 @@ func main() {
 		c = math.Cos(t)
 		s = math.Sin(t)
 
-		model := mgl32.Mat4{
-			float32(math.Pow(c, 2)), float32(-c * s), float32(s), 0.0,
-			float32(c * (math.Pow(s, 2) + s)), float32(math.Pow(c, 2) - math.Pow(s, 3)), float32(-c * s), 0.0,
-			float32(s * (s - math.Pow(c, 2))), float32(c * (math.Pow(s, 2) + s)), float32(math.Pow(c, 2)), 0.0,
-			-3.0, 0.0, 0.0, 1.0,
-		}
-
-		gl.UniformMatrix4fv(projectionID, 1, false, &projection[0])
-		gl.UniformMatrix4fv(viewID, 1, false, &view[0])
-		gl.UniformMatrix4fv(modelID, 1, false, &model[0])
-		gl.Uniform3fv(uLightID, 1, &uLight[0])
-
-		gl.ActiveTexture(gl.TEXTURE0)
-		gl.BindTexture(gl.TEXTURE_2D, myObject.TextureDiff)
-		gl.Uniform1i(textureDiffID, 0)
-
-		gl.ActiveTexture(gl.TEXTURE1)
-		gl.BindTexture(gl.TEXTURE_2D, myObject.TextureNorm)
-		gl.Uniform1i(textureNormID, 1)
-
-		vertAttrib := uint32(gl.GetAttribLocation(program, gl.Str("position\x00")))
-		gl.BindBuffer(gl.ARRAY_BUFFER, vboV)
-		gl.BufferData(gl.ARRAY_BUFFER, len(myObject.VertexArray)*4, gl.Ptr(myObject.VertexArray), gl.STATIC_DRAW)
-		gl.EnableVertexAttribArray(vertAttrib)
-		gl.VertexAttribPointer(vertAttrib, 3, gl.FLOAT, false, 0, gl.PtrOffset(0))
-
-		txtAttrib := uint32(gl.GetAttribLocation(program, gl.Str("tex_coords\x00")))
-		gl.BindBuffer(gl.ARRAY_BUFFER, vboT)
-		gl.BufferData(gl.ARRAY_BUFFER, len(myObject.TextureArray)*4, gl.Ptr(myObject.TextureArray), gl.STATIC_DRAW)
-		gl.EnableVertexAttribArray(txtAttrib)
-		gl.VertexAttribPointer(txtAttrib, 2, gl.FLOAT, false, 0, gl.PtrOffset(0))
-
-		normAttrib := uint32(gl.GetAttribLocation(program, gl.Str("normal\x00")))
-		gl.BindBuffer(gl.ARRAY_BUFFER, vboN)
-		gl.BufferData(gl.ARRAY_BUFFER, len(myObject.NormalArray)*4, gl.Ptr(myObject.NormalArray), gl.STATIC_DRAW)
-		gl.EnableVertexAttribArray(normAttrib)
-		gl.VertexAttribPointer(normAttrib, 3, gl.FLOAT, false, 0, gl.PtrOffset(0))
-
-		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(myObject.VertexArray)))
-
-		window.SwapBuffers()
-		glfw.PollEvents()
-
-		gl.DisableVertexAttribArray(vertAttrib)
-		gl.DisableVertexAttribArray(txtAttrib)
-		gl.DisableVertexAttribArray(normAttrib)
 	}
+
+	for _, obj := range objects {
+		gl.DeleteBuffers(1, &obj.Vertexbuffer)
+		gl.DeleteBuffers(1, &obj.Uvbuffer)
+		gl.DeleteBuffers(1, &obj.Unbuffer)
+		gl.DeleteBuffers(1, &obj.VertexArrayID)
+	}
+
 }
 
 func setupScene() {
--- programs/golang/project/object/object.go
+++ programs/golang/project/object/object.go
@@ -2,24 +2,56 @@ package object
 
 import (
 	_ "fmt"
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/mathgl/mgl32"
+	"math"
 )
 
 type Object struct {
-	NormalArray  []float32 // light vector coords (1 per side)
-	VertexArray  []float32 // vertices coords
-	TextureArray []float32 // texture coords
-	TextureDiff  uint32    // texture diffuse
-	TextureNorm  uint32    // texture normal map
-	Program      uint32    // shader program
+	NormalArray   []float32 // light vector coords (1 per side)
+	VertexArray   []float32 // vertices coords
+	TextureArray  []float32 // texture coords
+	Model         mgl32.Mat4
+	VertexArrayID uint32
+	Vertexbuffer  uint32
+	Uvbuffer      uint32
+	Unbuffer      uint32
+	X             float32
+	Y             float32
+	Z             float32
 }
 
-func New(VertexArray, TextureArray, NormalArray []float32, textureDiff, textureNorm, program uint32) *Object {
+func New(VertexArray, TextureArray, NormalArray []float32) *Object {
 	object := new(Object)
 	object.VertexArray = VertexArray
 	object.TextureArray = TextureArray
 	object.NormalArray = NormalArray
-	object.TextureDiff = textureDiff
-	object.TextureNorm = textureNorm
-	object.Program = program
 	return object
+}
+
+func (o Object) InitGlObject(object *Object) {
+	// VERTEX DATA CONFIGURE
+	gl.GenVertexArrays(1, &object.VertexArrayID)
+	gl.BindVertexArray(object.VertexArrayID)
+
+	gl.GenBuffers(1, &object.Vertexbuffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, object.Vertexbuffer)
+	gl.BufferData(gl.ARRAY_BUFFER, len(object.VertexArray)*4, gl.Ptr(object.VertexArray), gl.STATIC_DRAW)
+
+	gl.GenBuffers(1, &object.Uvbuffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, object.Uvbuffer)
+	gl.BufferData(gl.ARRAY_BUFFER, len(object.TextureArray)*4, gl.Ptr(object.TextureArray), gl.STATIC_DRAW)
+
+	gl.GenBuffers(1, &object.Unbuffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, object.Unbuffer)
+	gl.BufferData(gl.ARRAY_BUFFER, len(object.NormalArray)*4, gl.Ptr(object.NormalArray), gl.STATIC_DRAW)
+}
+
+func (o Object) InitGlObjectModel(object *Object, c, s float64) {
+	object.Model = mgl32.Mat4{
+		float32(math.Pow(c, 2)), float32(-c * s), float32(s), 0.0,
+		float32(c * (math.Pow(s, 2) + s)), float32(math.Pow(c, 2) - math.Pow(s, 3)), float32(-c * s), 0.0,
+		float32(s * (s - math.Pow(c, 2))), float32(c * (math.Pow(s, 2) + s)), float32(math.Pow(c, 2)), 0.0,
+		object.X, object.Y, object.Z, 1.0,
+	}
 }
